14b8761407d12a9e8129aa7c90bb97c9d52ccaf0
wpw3636 <will.wag812@gmail.com>
2025-11-01 21:30:42 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+﻿hour = int(input("Starting time (hours): "))
+mins = int(input("Starting time (minutes): "))
+dura = int(input("Event duration (minutes): "))
+
+hdura = int(dura//60)
+mdura = int(dura%60)
+
+ohour = hour + hdura
+omin = mins + mdura
+
+if omin > 59:
+    if ohour > 23:
+        print(str(ohour%24+1)+":"+str(omin%60))
+    else:
+        print(str(ohour+1)+":"+str(omin%60))
+else:
+    if ohour > 23:
+        print(str(ohour%24)+":"+str(omin))
+    else:
+        print(str(ohour)+":"+str(omin))
