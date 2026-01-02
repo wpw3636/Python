90cebce6dfb1f6820d8b5dae8fbaefe2b6fa4a27
wpw3636 <will.wag812@gmail.com>
2026-01-02 17:48:52 -0500

    Add files via upload

---
+++ impliedvol.py
@@ -0,0 +1,15 @@
+import numpy as np
+import yfinance as yf
+import pandas as pd
+
+def historical_vol(ticker):
+    data = yf.download(ticker, start="2025-01-01", end="2025-12-31")
+    daily_change = (data['Close'] - data['Open'])/data['Open']
+    daily_standev = (np.std(daily_change, axis = 0))[ticker]
+    year_standev = (daily_standev * np.sqrt(len(data)))
+    monthly_standev = (year_standev / np.sqrt(12))
+    print(ticker, "2025 daily volatility:", round(daily_standev * 100, 2),"%")
+    print(ticker, "2025 monthy volatility:", round(monthly_standev * 100, 2),"%")
+    print(ticker, "2025 annualized volatility:", round(year_standev * 100, 2),"%")
+
+historical_vol('AAPL')
