49019347ab152f6b0f68352923d1087e10bd359c
Will Wagner <will.wag812@gmail.com>
2026-01-05 14:04:35 -0500

    Add files via upload

---
+++ collatz.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numberOfSteps(self, num):
+        steps = 0
+        while num != 0:
+            if num%2 == 0:
+                num /= 2
+                steps += 1
+            else:
+                num -= 1
+                steps += 1
+        return steps
+
+num = 14
+sol = Solution()
+print(sol.numberOfSteps(num))
