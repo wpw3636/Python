66cd3f17c4d2e4a34a9af32fe0880e5649bff473
wpw3636 <will.wag812@gmail.com>
2026-01-05 13:36:37 -0500

    Update and rename options.py to BlackScholes.py

--- BlackScholes.py
+++ BlackScholes.py
@@ -7,7 +7,6 @@ def black_scholes_call(S, K, T, r, sigma):
     C = S * norm.cdf(d1) - K * np.e ** (-r * T) * norm.cdf(d2)
     return C
 
-# Inputs
 S = 180
 K = 185
 T = 30 / 365
@@ -16,14 +15,3 @@ sigma = 0.25
 
 price = black_scholes_call(S, K, T, r, sigma)
 print(f"Call Option Price: ${price:.2f}")
-
-import matplotlib.pyplot as plt
-
-vols = np.linspace(0.1, 0.6, 20) # What does linspace do?
-prices = [black_scholes_call(S, K, T, r, v) for v in vols]
-
-plt.plot(vols, prices)
-plt.xlabel("Volatility")
-plt.ylabel("Call Option Price")
-plt.title("Call Option Price vs Volatility")
-plt.show()
