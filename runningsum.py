c713affcce45cc9b19bbd8e55a7796720f271375
Will Wagner <will.wag812@gmail.com>
2026-01-05 14:15:14 -0500

    Update runningsum.py

--- runningsum.py
+++ runningsum.py
@@ -1,12 +1,9 @@
-class Solution:
-    def runningSum(self, nums):
-        runningsum = []
-        num = 0
-        for n in nums:
-            num += n
-            runningsum.append(num)
-        return runningsum  
-    
+def runningSum(nums):
+    runningsum = []
+    num = 0
+    for n in nums:
+        num += n
+        runningsum.append(num)
+    return runningsum  
 nums = [1,2,3,4]
-sol = Solution()
-print(sol.runningSum(nums))
+print(runningSum(nums))
